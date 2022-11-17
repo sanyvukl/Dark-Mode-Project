b80c0e549e9bd99337a053250a66d8a61e77203c
Alex <vukliuk@MacBook-Pro-Vukliuk.local>
2022-11-17 12:43:52 -0800

    Little upgrade js

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 window.onload = function () {
+  const MODE_TYPES = {
+    LIGHT_MODE: "LIGHT_MODE",
+    DARK_MODE: "DARK_MODE",
+  };
+
   const { documentElement } = document;
   const toggleSwitch = document.getElementById("theme-button");
   const toggleText = document.getElementById("toggle-text");
   const toggleIcon = document.getElementById("toggle-icon");
-  const nav = document.getElementById("nav");
 
   const image1 = document.getElementById("image1");
   const image2 = document.getElementById("image2");
   const image3 = document.getElementById("image3");
 
   const imageMode = (mode) => {
-    if (mode === "light") {
+    if (mode === MODE_TYPES.LIGHT_MODE) {
       image1.setAttribute("src", "img/undraw_proud_coder_light.svg");
       image2.setAttribute("src", "img/undraw_feeling_proud_light.svg");
       image3.setAttribute("src", "img/undraw_conceptual_idea_light.svg");
       return;
     }
-    if (mode === "dark") {
+    if (mode === MODE_TYPES.DARK_MODE) {
       image1.setAttribute("src", "img/undraw_proud_coder_dark.svg");
       image2.setAttribute("src", "img/undraw_feeling_proud_dark.svg");
       image3.setAttribute("src", "img/undraw_conceptual_idea_dark.svg");
@@ -28,14 +32,14 @@ window.onload = function () {
     toggleText.textContent = "Light Mode";
     toggleIcon.classList.replace("fa-moon", "fa-sun");
 
-    imageMode("light");
+    imageMode(MODE_TYPES.LIGHT_MODE);
   };
   const darkMode = () => {
     documentElement.setAttribute("data-theme", "dark");
     toggleText.textContent = "Dark Mode";
     toggleIcon.classList.replace("fa-sun", "fa-moon");
 
-    imageMode("dark");
+    imageMode(MODE_TYPES.DARK_MODE);
   };
 
   const switchTheme = (event) => {
