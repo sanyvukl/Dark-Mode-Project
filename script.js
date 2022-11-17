f0584008953445ab8d0b75b1cef695e0d54182b9
Alex <vukliuk@MacBook-Pro-Vukliuk.local>
2022-11-17 12:40:46 -0800

    Upgraded styles

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
 window.onload = function () {
+  const { documentElement } = document;
   const toggleSwitch = document.getElementById("theme-button");
   const toggleText = document.getElementById("toggle-text");
   const toggleIcon = document.getElementById("toggle-icon");
-
   const nav = document.getElementById("nav");
 
   const image1 = document.getElementById("image1");
   const image2 = document.getElementById("image2");
   const image3 = document.getElementById("image3");
-  const textBox = document.getElementById("text-box");
 
-  const lightMode = () => {
-    nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
-
-    document.documentElement.setAttribute("data-theme", "light");
-    toggleText.textContent = "Light Mode";
-    toggleIcon.classList.remove("fa-moon");
-    toggleIcon.classList.add("fa-sun");
-
-    image1.setAttribute("src", "img/undraw_proud_coder_light.svg");
-    image2.setAttribute("src", "img/undraw_feeling_proud_light.svg");
-    image3.setAttribute("src", "img/undraw_conceptual_idea_light.svg");
-
-    textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
-    textBox.style.color = "white";
+  const imageMode = (mode) => {
+    if (mode === "light") {
+      image1.setAttribute("src", "img/undraw_proud_coder_light.svg");
+      image2.setAttribute("src", "img/undraw_feeling_proud_light.svg");
+      image3.setAttribute("src", "img/undraw_conceptual_idea_light.svg");
+      return;
+    }
+    if (mode === "dark") {
+      image1.setAttribute("src", "img/undraw_proud_coder_dark.svg");
+      image2.setAttribute("src", "img/undraw_feeling_proud_dark.svg");
+      image3.setAttribute("src", "img/undraw_conceptual_idea_dark.svg");
+      return;
+    }
   };
+  const lightMode = () => {
+    documentElement.setAttribute("data-theme", "light");
+    toggleText.textContent = "Light Mode";
+    toggleIcon.classList.replace("fa-moon", "fa-sun");
 
+    imageMode("light");
+  };
   const darkMode = () => {
-    nav.style.backgroundColor = "rgb(1 1 1 / 50%)";
-    textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
-
-    document.documentElement.setAttribute("data-theme", "dark");
+    documentElement.setAttribute("data-theme", "dark");
     toggleText.textContent = "Dark Mode";
-    toggleIcon.classList.remove("fa-sun");
-    toggleIcon.classList.add("fa-moon");
+    toggleIcon.classList.replace("fa-sun", "fa-moon");
 
-    image1.setAttribute("src", "img/undraw_proud_coder_dark.svg");
-    image2.setAttribute("src", "img/undraw_feeling_proud_dark.svg");
-    image3.setAttribute("src", "img/undraw_conceptual_idea_dark.svg");
-
-    textBox.style.color = "black";
+    imageMode("dark");
   };
 
   const switchTheme = (event) => {
